171617fbb3ea176c2ef16c71ca162698cb66c5f6
Sangwon Lim <angwonlim@gmail.com>
2022-02-03 10:10:35 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+__version__ = (0, 0, 0)
+
+setup(
+    name="sea_ice_phenology",
+    description="Sea Ice Phenology Indicator Extraction from Remote Sensing Data",
+    version=".".join(str(d) for d in __version__),
+    author="Sangwon Lim",
+    author_email="sangwonl@uvic.ca",
+    packages=["sea_ice_phenology"],
+    include_package_data=True,
+    scripts="""
+        ./scripts/
+    """.split(),
+)
