af8a16567e8b9b34f17392c05898cffd530e7d95
Sangwon Lim <angwonlim@gmail.com>
2022-02-10 13:41:30 -0800

    add url to the repo

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ __version__ = (0, 0, 0)
 setup(
     name="sea_ice_phenology",
     description="Sea Ice Phenology Indicator Extraction from Remote Sensing Data",
+    url="https://github.com/sum1lim/rs_tools",
     version=".".join(str(d) for d in __version__),
     author="Sangwon Lim",
     author_email="sangwonl@uvic.ca",
