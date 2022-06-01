5cf207c65b7742238f18b6b121497a6fd1defd49
Sangwon Lim <sangwonl@uvic.ca>
2022-05-31 20:24:41 -0600

    refine scripts

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ __version__ = (0, 0, 0)
 setup(
     name="sea_ice_phenology",
     description="Sea Ice Phenology Indicator Extraction from Remote Sensing Data",
-    url="https://github.com/sum1lim/rs_tools",
+    url="https://github.com/sum1lim/sea-ice-phenology",
     version=".".join(str(d) for d in __version__),
     author="Sangwon Lim",
-    author_email="sangwonl@uvic.ca",
+    author_email="sangwon3@ualberta.ca",
     packages=["sea_ice_phenology"],
     include_package_data=True,
     scripts="""
